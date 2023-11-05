4af10d4758d4b0f086a6cbd37c7c876a3fe6a98c
laxmi124 <duttasuravi225@gmail.com>
2023-11-05 18:49:56 +0530

    profile photo updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import myImg from "../../Assets/updatedProfileImage.png";
 
 function Home() {
   return (
@@ -32,7 +32,7 @@ function Home() {
 
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
-                src={homeLogo}
+                src={myImg}
                 alt="home pic"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
